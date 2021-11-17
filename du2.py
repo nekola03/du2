0366525fc7d94b9ddbcb800889ad70d3a0abee42
Lukáš Nekola <nekola.luky@seznam.cz>
2021-11-17 22:14:43 +0100

    commit 3

--- du2.py
+++ du2.py
@@ -4,35 +4,23 @@ import numpy
 
 def remoteWrongData (dataInput):    #řadit pod sebe bez čárek
     for row in dataInput:  
-        x = []
         try:
-            #writer.writerow([float(row[5])])
-            all.append([float(row[5])])
+            writer.writerow([row[0],row[1],row[2],row[3],row[4],float(row[5])])
         except ValueError:
             pass   
- 
-def averageSevenData (dataInput):
-    steps = 3
-    print(len(dataInput))
+
+#def choiceFifthColumn (dataInput): #zde bude vstup z remoteWrongData
+
+#def averageSevenData (dataInput): #zde bude vstup z choice FifthColumn
     
-def choiceSevenData (dataInput):
+def choiceSevenData (dataInput): #zde bude vstup z remoteWrongData
     seventhLine = itertools.islice(dataInput, 0, None, 7)   #součást itertools
     for row in seventhLine:    
         writer.writerow([row[0],row[1],row[3],row[4]])     
+
+#def mergeWeekData (dataInput): #spojit choiceSevenData a averageSevenData
             
 with open ("Data_zelivka.csv", encoding="utf8") as csvZelivkaInput,\
      open("result.csv","w", encoding="utf8") as vysledek1:
     dataInput = csv.reader(csvZelivkaInput, delimiter = ",")
     writer = csv.writer(vysledek1)
-    all = []
-    for row in dataInput:  
-        all.append(row[5])
-    print(all)
-    """
-    seventhLine = itertools.islice(dataInput, 0, None, 7)   #součást itertools
-    for row in seventhLine:    
-        writer.writerow([row[0], row[1], row[2], row[3], row[4]])
-        """
-    
-    
-    

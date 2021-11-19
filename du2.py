d09c271e79095a1a6504558a24d84ada2220649d
Lukáš Nekola <nekola.luky@seznam.cz>
2021-11-19 21:21:02 +0100

    commit 6

--- du2.py
+++ du2.py
@@ -2,32 +2,36 @@ import csv
 import itertools
 import numpy
 
+
             
 with open ("Data_zelivka.csv", encoding="utf8") as csvZelivkaInput,\
-     open("result.csv","w", encoding="utf8") as vysledek1:
+     open("result1.csv","w", encoding="utf8") as vysledek1:
     dataInput = csv.reader(csvZelivkaInput, delimiter = ",")
-    writer = csv.writer(vysledek1)
-    numRow = -1
+    writer = csv.writer(vysledek1)   
+    numRow = 0
+    aveSevenDay = 0
     for row in dataInput:
         numRow = numRow + 1
+        print(numRow)
         choiceSevenDay = numRow % 7
+        try:
+            aveSevenDay += float(row[-1])
+        except ValueError:
+            pass
         if choiceSevenDay == 0:
-            i = 0
-            for row in range(7):
-                try:
-                    i = sum(float(row[5]))
-                    print(row[0],row[1],row[2],row[3],row[4],i)
-                except:
-                    pass
-
-            
-            
-            
-            
-            #try:
-            #    print(row[0],row[1],row[2],row[3],row[4],float(row[5]))
-            #except ValueError:
-            #    pass
-
-
-            
+            aveSevenDay = aveSevenDay/7
+            writer.writerow([row[0],row[1],row[2],row[3],row[4],float(aveSevenDay)])
+    """
+    minTemp = [] 
+    maxTemp = [] 
+    for row in dataInput:
+        try:
+            minTemp.append([row[2],row[3],row[4],float(row[5])])
+            maxTemp.append([row[2],row[3],row[4],float(row[5])])
+        except:
+            pass
+    theindexmin=minTemp.index(min(minTemp))
+    theindexmax=maxTemp.index(max(maxTemp))
+    print("Minimální hodnota: \n", minTemp[theindexmin]) 
+    print("\n\nMaximální hodnota: \n",maxTemp[theindexmax]) 
+        """

14876677aeccbea8cd1c4aa20a9323554ca7d456
Lukáš Nekola <nekola.luky@seznam.cz>
2021-11-20 14:24:39 +0100

    commit 8

--- du2.py
+++ du2.py
@@ -1,34 +1,50 @@
 import csv
-import itertools
-import numpy
   
-with open ("Data_zelivka.csv", encoding="utf8") as csvZelivkaInput,\
-     open("result1.csv","w", encoding="utf8") as vysledek1:
-    dataInput = csv.reader(csvZelivkaInput, delimiter = ",")
-    writerWeek = csv.writer(vysledek1)   
-    rowNumber = -1
+with open ("Data_zelivka.csv", encoding="utf8") as csvInput,\
+     open("vystup_7dni.csv","w", encoding="utf8") as csvOutput:
+    dataInput = csv.reader(csvInput, delimiter = ",")
+    writerWeek = csv.writer(csvOutput)   
+    writerYear = csv.writer(csvOutput)  
+    rowNumber = 0
     avSevenDay = 0
-    for row in dataInput:
-        rowNumber += 1
-        if rowNumber%7 == 0:
+    for row in dataInput:     
+        if rowNumber % 7 == 0:
             firstDayWeek = row
-        rowNumberOneBig = rowNumber + 1
-        avSevenDay += float(row[5])/7
-        if rowNumberOneBig%7 ==0:
-            firstDayWeek[5] = float(avSevenDay)
-            print(firstDayWeek)
-            avSevenDay = 0
+        try:
+            avSevenDay += float(row[5])/7
+        except ValueError:
+            print("Na řádku ",dataInput.line_num ," je chybně zadaná hodnota, a proto ji program nezapočítá")
+        except:
+            print("Na řádku ",dataInput.line_num ," je nesprávně dlouhé pole a proto ji program nezapočítá")
+        print(dataInput.line_num)
+        if (rowNumber % 7 == 6):
+            firstDayWeek[5] = avSevenDay
+            writerWeek.writerow(firstDayWeek)
+            avSevenDay -= avSevenDay
+        rowNumber += 1
+        
             
 
     
 
 """
-with open ("Data_zelivka.csv", encoding="utf8") as csvZelivkaInput:
-    dataInput = csv.reader(csvZelivkaInput, delimiter = ",")
-    rowsmax = max(dataInput, key=lambda row: float(row[5]))
+with open ("Data_zelivka.csv", encoding="utf8") as csvInput:
+    dataInput = csv.reader(csvInput, delimiter = ",")
+    for row in dataInput:
+        par = row
+        try:
+            par[5] = float(row[5])
+        except:
+            pass
+        
+    rowsmax = max(dataInput, key=lambda row: par[5])
     print(rowsmax)
-with open ("Data_zelivka.csv", encoding="utf8") as csvZelivkaInput:
-    dataInput = csv.reader(csvZelivkaInput, delimiter = ",")
-    rowsmin = min(dataInput, key=lambda row: float(row[5]))
-    print(rowsmin)
+    """
+
 """
+with open ("Data_zelivka.csv", encoding="utf8") as csvInput:
+    dataInput = csv.reader(csvInput, delimiter = ",")
+    rowsmin = min(dataInput, key=lambda row: row[5])
+    print(rowsmin)
+    """
+    

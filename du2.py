c9e3135338e2dc905b481b963a7a9fe17f414007
Lukáš Nekola <nekola.luky@seznam.cz>
2021-11-21 15:01:54 +0100

    commit 9

--- du2.py
+++ du2.py
@@ -6,45 +6,47 @@ with open ("Data_zelivka.csv", encoding="utf8") as csvInput,\
     writerWeek = csv.writer(csvOutput)   
     writerYear = csv.writer(csvOutput)  
     rowNumber = 0
+    suma = 0
     avSevenDay = 0
+    residue = 0
     for row in dataInput:     
         if rowNumber % 7 == 0:
-            firstDayWeek = row
+            firstDayWeek = row           
         try:
-            avSevenDay += float(row[5])/7
+            avSevenDay += float(row[5])
+            residue += 1
         except ValueError:
             print("Na řádku ",dataInput.line_num ," je chybně zadaná hodnota, a proto ji program nezapočítá")
         except:
             print("Na řádku ",dataInput.line_num ," je nesprávně dlouhé pole a proto ji program nezapočítá")
-        print(dataInput.line_num)
         if (rowNumber % 7 == 6):
+            avSevenDay = avSevenDay / 7
+            avSevenDay = "{:.4f}".format(avSevenDay)
             firstDayWeek[5] = avSevenDay
             writerWeek.writerow(firstDayWeek)
+            residue -= residue
+            avSevenDay = float(avSevenDay)
             avSevenDay -= avSevenDay
         rowNumber += 1
-        
-            
+    if (rowNumber % 7 != 6):
+        avSevenDay = avSevenDay / residue
+        avSevenDay = "{:.4f}".format(avSevenDay)
+        firstDayWeek[5] = avSevenDay
+        writerWeek.writerow(firstDayWeek)
 
     
 
-"""
+
+"""        
 with open ("Data_zelivka.csv", encoding="utf8") as csvInput:
     dataInput = csv.reader(csvInput, delimiter = ",")
-    for row in dataInput:
-        par = row
-        try:
-            par[5] = float(row[5])
-        except:
-            pass
-        
-    rowsmax = max(dataInput, key=lambda row: par[5])
+    rowsmax = max(dataInput, key=lambda row: row[5])
     print(rowsmax)
-    """
 
-"""
 with open ("Data_zelivka.csv", encoding="utf8") as csvInput:
     dataInput = csv.reader(csvInput, delimiter = ",")
     rowsmin = min(dataInput, key=lambda row: row[5])
     print(rowsmin)
     """
+
     
